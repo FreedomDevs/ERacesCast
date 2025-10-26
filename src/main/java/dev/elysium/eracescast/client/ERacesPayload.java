63cc73f6012fc6d7f7f842d97eafff77c6fb9e1b
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-10-26 16:27:08 +0300

    По идее мод завершён

---
+++ src/main/java/dev/elysium/eracescast/client/ERacesPayload.java
@@ -0,0 +1,23 @@
+package dev.elysium.eracescast.client;
+
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
+
+public record ERacesPayload(Long timestamp, String type, String payload) implements CustomPayload {
+    public static final CustomPayload.Id<ERacesPayload> ID = new CustomPayload.Id<>(Identifier.of("elysium", "eraces"));
+
+    public static final PacketCodec<RegistryByteBuf, ERacesPayload> CODEC = PacketCodec.tuple(
+            PacketCodecs.LONG, ERacesPayload::timestamp,
+            PacketCodecs.STRING, ERacesPayload::type,
+            PacketCodecs.STRING, ERacesPayload::payload,
+            ERacesPayload::new
+    );
+
+    @Override
+    public CustomPayload.Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+}
--- src/main/java/dev/elysium/eracescast/client/EracescastClient.java
+++ src/main/java/dev/elysium/eracescast/client/EracescastClient.java
@@ -3,11 +3,22 @@ package dev.elysium.eracescast.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class EracescastClient implements ClientModInitializer {
 
@@ -16,11 +27,30 @@ public class EracescastClient implements ClientModInitializer {
 
     private boolean isCastEnabled = false;
     private long castEnableTime = 0;
+    private int lockedslot = 0;
+    private Set<Integer> lastPressd = new HashSet<>();
+    private List<Integer> keys = new ArrayList<>();
 
     public void castEnd() {
         isCastEnabled = false;
-        System.out.println("Каст завершён");
+        System.out.println("Каст завершён с "+keys);
+        MinecraftClient.getInstance().player.sendMessage(Text.literal("Каст завершён с "+keys), false);
+        sendEndCast();
+    }
 
+    public void sendStartCast() {
+        ERacesPayload payload = new ERacesPayload(System.currentTimeMillis(), "start_cast", "");
+        ClientPlayNetworking.send(payload);
+    }
+    public void sendEndCast() {
+        ERacesPayload payload = new ERacesPayload(System.currentTimeMillis(), "end_cast", keys.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining()));
+        ClientPlayNetworking.send(payload);
+    }
+    public void sendKey(int key) {
+        ERacesPayload payload = new ERacesPayload(System.currentTimeMillis(), "cast_key", String.valueOf(key));
+        ClientPlayNetworking.send(payload);
     }
 
     @Override
@@ -31,8 +61,10 @@ public class EracescastClient implements ClientModInitializer {
                 GLFW.GLFW_KEY_G,
                 "category.elysium.keys"
         ));
-
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (!ClientPlayNetworking.canSend(Identifier.of("elysium", "eraces")))
+                return;
+
             if (myKey.wasPressed()) {
                 isHolding = true;
             } else if (isHolding) {
@@ -40,13 +72,35 @@ public class EracescastClient implements ClientModInitializer {
                 if (!isCastEnabled) {
                     isCastEnabled = true;
                     castEnableTime = System.currentTimeMillis();
+                    keys = new ArrayList<>();
+                    if (client.player != null)
+                        lockedslot = client.player.getInventory().selectedSlot;
+                    sendStartCast();
                     System.out.println("Каст запущен");
                 } else if (System.currentTimeMillis() - castEnableTime > 100)
                         castEnd();
             }
 
-            if (isCastEnabled && System.currentTimeMillis() - castEnableTime > 1000 * 3)
+            if (isCastEnabled && System.currentTimeMillis() - castEnableTime > 1000 * 10)
                 castEnd();
+
+            if (isCastEnabled) {
+                if (client.player != null)
+                    client.player.getInventory().selectedSlot = lockedslot;
+
+                for (int i = 0; i < 9; i++) {
+                    int num = i + 1;
+                    int key = GLFW.GLFW_KEY_1 + i; // 1..9
+                    if (InputUtil.isKeyPressed(client.getWindow().getHandle(), key)) {
+                        lastPressd.add(num);
+                    } else if (lastPressd.contains(num)) {
+                        sendKey(num);
+                        keys.add(num);
+                        lastPressd.remove(num);
+                        System.out.println("Нажата клавиша: " + num);
+                    }
+                }
+            }
         });
 
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
@@ -56,22 +110,23 @@ public class EracescastClient implements ClientModInitializer {
             int x1 = 7;
             int y1 = 7;
 
-            int color = 0xFF662244;
-            int color1 = 0x80AAAAAA;
-            int color2 = 0xFFFF0000;
+            int textColor = 0xFF662244;
+            int backgroundColor = 0x80AAAAAA;
+            int progressBarColor = 0xFFAAAA00;
 
             if (isCastEnabled) {
-                drawContext.fill(x1 - 3, y1 - 3, x1 + 65, y1 + 11, color1);
-                drawContext.drawText(client.textRenderer, "Режим каста", x1, y1, color, false);
+                drawContext.fill(x1 - 3, y1 - 3, x1 + 65, y1 + 11, backgroundColor);
+                drawContext.drawText(client.textRenderer, "Режим каста", x1, y1, textColor, false);
 
                 long timeToEnd = System.currentTimeMillis() - castEnableTime;
                 long min = 0;
-                long max = 3 * 1000;
+                long max = 10 * 1000;
 
                 double progress = (double) (timeToEnd - min) / (max - min);
 
-                drawContext.fill(x1 - 1, y1 + 10, x1 + (63 - (int) (63 * progress)), y1 + 9, color2);
+                drawContext.fill(x1 - 1, y1 + 9, x1 + (63 - (int) (63 * progress)), y1 + 10, progressBarColor);
             }
         });
+
     }
 }
